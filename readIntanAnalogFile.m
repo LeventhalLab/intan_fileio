90d54bd1a53f684694922f329c0e4249d7639876
jmagnu1 <54630297+jmagnu1@users.noreply.github.com>
2020-04-07 16:17:45 -0400

    Add files via upload

--- readIntanAnalogFile.m
+++ readIntanAnalogFile.m
@@ -1,12 +1,13 @@
-function v = readIntanAnalogFile(fname,board_adc_channels)
+function analog_data = readIntanAnalogFile(fname, board_adc_channels)
 %
 % function to read in a digital IO Intan file ('digitalIn' or 'digitalOut')
 %
 % INPUTS
 %   fname - filename of the Intan digital file to read
-%
+%   board_adc_channels - board adc channel structure from
+%       read_Intan_RHD2000_file_DL
 % OUTPUTS
-%   v - m x n matrix where each row is a vector of voltages recorded on
+%   analog_data - m x n matrix where each row is a vector of voltages recorded on
 %       a single channel
 
 num_channels = length(board_adc_channels); % ADC input info from header file
@@ -14,7 +15,6 @@ fileinfo = dir(fname);
 num_samples = fileinfo.bytes/(num_channels * 2); % uint16 = 2 bytes
 fid = fopen(fname, 'r');
 
-v = fread(fid, [num_channels, num_samples], 'uint16');
+analog_data = fread(fid, [num_channels, num_samples], 'uint16');
 fclose(fid);
-v = v * 0.000050354; % convert to volts
-
+v = analog_data * 0.000050354; % convert to volts
--- readIntanDigitalFile.m
+++ readIntanDigitalFile.m
@@ -1,4 +1,4 @@
-function digital_word = readIntanDigitalFile(fname, varargin)
+function digital_data = readIntanDigitalFile(fname, varargin)
 %
 % function to read in a digital IO Intan file ('digitalIn' or 'digitalOut')
 % if Varargins are not specified, assume user wants to read the entire file
@@ -12,8 +12,10 @@ function digital_word = readIntanDigitalFile(fname, varargin)
 %   samples_to_read - number of samples to read in. Again, this is in
 %       samples, NOT in bytes
 %
+%   frequency - sample_rate(in Hz, default = 20kHz aka 20000Hz)
+% 
 % OUTPUTS
-%   digital_word - vector of uint16's containing bit-wise values of each
+%   digital_data - vector of uint16's containing bit-wise values of each
 %      digital line from the Intan system
 
 if nargin > 1
@@ -30,9 +32,9 @@ end
 fid = fopen(fname, 'r');
 
 fseek(fid, start_byte, 'bof');
-digital_word = fread(fid, samples_to_read, 'uint16');
+digital_data = fread(fid, samples_to_read, 'uint16');
 
 fclose(fid);
 
 % Individual digital inputs can be isolated using the bitand function in MATLAB:
-% digital_input_ch = (bitand(digital_word, 2^ch) > 0); % ch has a value of 0-15 here
+% digital_input_ch = (bitand(digital_data, 2^ch) > 0); % ch has a value of 0-15 here
